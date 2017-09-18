da7a0dbe19d231444f4bed7163c9ae298265eb76
kirillshah95 <kirillshah95@gmail.com>
2017-09-18 21:47:12 +0500

    add ajax

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/hydra1isk.json',
+    dataType: 'jsonp',
+    success: function(response) {
+        let badgeLength = response.courses.completed.length;
+        console.log(response.courses.completed);
+        for(let i = 0; i < badgeLength; i++){
+            $('#badges').append('<div class="course"><h3>' + response.courses.completed[i].title + '</h3><img src="' + response.courses.completed[i].badge + '"/><a href="' + response.courses.completed[i].url + '" class="btn btn-primary" target="_blank">See Course</a></div>');
+        }
+    }
+  });
 
 });
